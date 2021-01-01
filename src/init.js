c285e1368c520a4a84db38f8213702aa664624c1
Ozgur <ozgr@live.com>
2021-01-01 16:51:48 +0300

    Add words and characters count

--- src/init.js
+++ src/init.js
@@ -1,1 +1,16 @@
-console.log('test')
+const wordsLengthSelector = document.getElementById('wordsLength')
+const charactersLengthSelector = document.getElementById('charactersLength')
+const facebookLengthSelector = document.getElementById('facebookLength')
+const twitterLengthSelector = document.getElementById('twitterLength')
+const googleLengthSelector = document.getElementById('googleLength')
+const inputSelector = document.getElementById('input')
+
+inputSelector.addEventListener('input', e => {
+  const value = e.target.value
+
+  const wordsLength = value.split(/[^\s*]\s+[^\s*]/).length
+  const charactersLength = value.length
+
+  wordsLengthSelector.innerHTML = wordsLength
+  charactersLengthSelector.innerHTML = charactersLength
+})
